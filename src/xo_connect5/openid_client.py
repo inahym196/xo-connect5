755ca2291f2822e643acf0aa86f48d8deb8f8dd6
inahym196 <user@USERnoMacBook-Pro.local>
2022-08-23 07:06:37 +0900

    omit-fetch-token-argument

--- src/xo_connect5/openid_client.py
+++ src/xo_connect5/openid_client.py
@@ -10,17 +10,17 @@ logger = logging.getLogger('uvicorn').getChild(__name__)
 class OpenIDClient:
     def __init__(self, app_client_id: str, app_client_secret: str, auth_realm_name: str, app_redirect_uri: str, auth_issuer_url: str) -> None:
 
+        meta_data_url = f'{auth_issuer_url}/realms/{auth_realm_name}/.well-known/openid-configuration'
+        meta_data = requests.get(url=meta_data_url).json()
+        self.auth_endpoint = meta_data['authorization_endpoint']
+
         self.client = OAuth2Session(
             client_id=app_client_id,
             client_secret=app_client_secret,
             scope='profile',
             redirect_uri=app_redirect_uri,
+            token_endpoint=meta_data['token_endpoint']
         )
-        meta_data_url = f'{auth_issuer_url}/realms/{auth_realm_name}/.well-known/openid-configuration'
-        meta_data = requests.get(url=meta_data_url).json()
-
-        self.auth_endpoint = meta_data['authorization_endpoint']
-        self.token_endpoint = meta_data['token_endpoint']
 
     def create_redirect_response(self) -> RedirectResponse:
         uri, state = self.client.create_authorization_url(url=self.auth_endpoint)
@@ -29,5 +29,5 @@ class OpenIDClient:
         return response
 
     def retrieve_token(self, code: str, state: str) -> str:
-        token = self.client.fetch_access_token(url=self.token_endpoint, code=code, state=state)
+        token = self.client.fetch_access_token(code=code, state=state)
         return token
