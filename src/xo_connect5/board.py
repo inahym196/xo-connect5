e6dc389d8d59e0c7492c302a28d5788e69e6e8e4
inahym196 <user@USERnoMacBook-Pro.local>
2022-08-28 17:08:14 +0900

    error-handling

--- src/xo_connect5/board.py
+++ src/xo_connect5/board.py
@@ -41,23 +41,22 @@ class RedisClient:
             return
 
         order = hget_bytes.decode()
-        if order in [OrderType.FIRST, OrderType.DRAW]:
-            return order
+        if order not in [OrderType.FIRST, OrderType.DRAW]:
+            return
+
+        return order
 
 
 async def get_user_order(user: str) -> Optional[OrderType]:
     redis_client = RedisClient()
     order = await redis_client.get_user_order(user)
-    if not order:
-        return
     return order
 
 
-async def get_piece_of_this_turn(order: Optional[OrderType] = Depends(get_user_order)) -> Optional[PieceType]:
+async def get_piece_of_this_turn(order: OrderType = Depends(get_user_order)) -> Optional[PieceType]:
     if not order:
         return
-
-    if order == OrderType.FIRST:
+    elif order == OrderType.FIRST:
         piece_type = PieceType.XP
     else:
         piece_type = PieceType.OG
@@ -70,20 +69,26 @@ class PutPieceParam:
         self.point = point
 
 
+class BoardError(Exception):
+    def __init__(self, detail: str) -> None:
+        super().__init__(detail)
+        self.detail = detail
+
+
 class Board:
     def __init__(self) -> None:
         none_piece = PieceType.NONE
         self.pieces = [[none_piece for j in range(LENGTH_OF_SIDE)] for i in range(LENGTH_OF_SIDE)]
         self.turn = 0
 
-    def put_piece(self, put_piece_param: PutPieceParam) -> bool:
+    def put_piece(self, put_piece_param: PutPieceParam) -> None:
         raw = put_piece_param.point.raw
         column = put_piece_param.point.column
+
         if not put_piece_param.piece_type:
-            return False
+            raise BoardError(detail='Piece type does not exist for the specified user')
         elif self.pieces[column][raw] != PieceType.NONE:
-            return False
+            raise BoardError(detail='A piece already exists at the specified location')
 
         self.pieces[column][raw] = put_piece_param.piece_type
         self.turn += 1
-        return True
