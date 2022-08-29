6f9100dbab74eb6c9c52ce42f3a70f5f617b3eb3
inahym196 <user@USERnoMacBook-Pro.local>
2022-08-30 02:29:40 +0900

    add-test

--- tests/test_boards.py
+++ tests/test_boards.py
@@ -1,5 +1,4 @@
 
-from typing import Any
 
 from fastapi.testclient import TestClient
 from requests.models import Response
@@ -11,21 +10,19 @@ from xo_connect5.models.users import Players, User
 client = TestClient(app)
 
 
-def test_read_boards():
+def test_get_boards():
     response: Response = client.get('/api/v1/boards')
-    response_dict: dict[str, Any] = response.json()
     expected = Boards()
     _players = Players(first=User(name='first'), draw=User(name='draw'))
     _board = Board(id=0, players=_players, status=BoardStatus.STARTING)
     expected.items.append(_board)
 
     assert response.status_code == 200
-    assert response_dict['items'] == expected.items
+    assert response.json()['items'] == expected.items
 
 
 def test_post_boards():
     response: Response = client.post('/api/v1/boards/')
-    response_dict: dict[str, Any] = response.json()
     expected = HTTPException(status_code=409)
     assert response.status_code == expected.status_code
-    assert response_dict['detail'] == expected.detail
+    assert response.json()['detail'] == expected.detail
---
+++ tests/test_pieces.py
@@ -0,0 +1,45 @@
+
+
+import pytest
+from fastapi.testclient import TestClient
+from requests.models import Response
+from xo_connect5.main import app
+from xo_connect5.models.boards import Board, BoardStatus
+from xo_connect5.models.pieces import Pieces, PieceType
+from xo_connect5.models.users import Players, User
+
+client = TestClient(app)
+
+
+@pytest.fixture
+def pieces():
+    pieces = [[PieceType.NONE for j in range(10)] for i in range(10)]
+    return pieces
+
+
+@pytest.fixture
+def board():
+    players = Players(first=User(name='first'), draw=User(name='draw'))
+    board = Board(id=0, players=players, status=BoardStatus.STARTING)
+    return board
+
+
+def test_get_pieces(pieces: Pieces):
+    response: Response = client.get('/api/v1/boards/0/pieces/')
+
+    assert response.status_code == 200
+    assert response.json() == pieces
+
+
+def test_put_pieces(pieces: Pieces, board: Board):
+    params = {'raw': 0, 'column': 0}
+    data = {
+        'user': {'name': 'first'},
+        'order': {'type': 'first'}
+    }
+    response: Response = client.patch('/api/v1/boards/0/pieces/', params=params, json=data)
+    pieces[0][0] = PieceType.XG
+    board.pieces = pieces
+
+    assert response.status_code == 200
+    assert response.json() == board
