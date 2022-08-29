36bf8ffc1061022a9bbda8a2d3e31922fd85ec18
inahym196 <user@USERnoMacBook-Pro.local>
2022-08-29 16:01:31 +0900

    implement-players-endpoint-and-restructure

--- src/xo_connect5/routers/boards/__init__.py
+++ src/xo_connect5/routers/boards/__init__.py
@@ -1,7 +1,9 @@
 
 from fastapi import APIRouter
-from xo_connect5.routers.boards import board, boards
+from xo_connect5.routers.boards import board, boards, pieces, players
 
 router = APIRouter()
 router.include_router(router=boards.router, tags=['boards'])
 router.include_router(router=board.router, prefix='/{board_id}', tags=['board'])
+router.include_router(router=pieces.router, prefix='/{board_id}/pieces', tags=['pieces'])
+router.include_router(router=players.router, prefix='/{board_id}/players', tags=['players'])
