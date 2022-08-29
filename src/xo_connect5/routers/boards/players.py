85782a7228b0c34d9a7d1316fcd47a3305bdcac5
inahym196 <user@USERnoMacBook-Pro.local>
2022-08-29 19:41:22 +0900

    change-order-path2body

--- src/xo_connect5/routers/boards/players.py
+++ src/xo_connect5/routers/boards/players.py
@@ -1,10 +1,9 @@
-
 from typing import Optional
 
 from fastapi import APIRouter, Depends
 from starlette.exceptions import HTTPException
 from xo_connect5.models.boards import Board
-from xo_connect5.models.users import OrderType, Player, Players
+from xo_connect5.models.users import Order, OrderType, Player, Players
 from xo_connect5.routers.boards.board import _get_board
 
 router = APIRouter()
@@ -21,37 +20,39 @@ async def get_players(board: Board = Depends(_get_board)) -> Players:
     return board.players
 
 
-def get_player_from_order_in_board(order: OrderType, board: Board) -> Optional[Player]:
-    sitting_player_dict = board.players.dict().get(order)
+def get_player_from_order_in_board(order: Order, board: Board) -> Optional[Player]:
+    sitting_player_dict = board.players.dict().get(order.type)
     if sitting_player_dict:
         return Player(**sitting_player_dict)
 
 
-@router.put('/{order}', response_model=Players)
-async def join_player(order: OrderType, param: PlayersParam = Depends()) -> Players:
+@router.put('/', response_model=Players)
+async def join_player(param: PlayersParam = Depends()) -> Players:
     join_player, board = param.player, param.board
+    order = join_player.order
     if order == OrderType.NONE:
         raise HTTPException(status_code=400, detail='Request order is none')
 
-    board_player = get_player_from_order_in_board(order, param.board)
+    board_player = get_player_from_order_in_board(order, board)
     if board_player:
         raise HTTPException(status_code=409, detail='Other player is on board')
 
-    board.players = board.players.copy(update={order: join_player}, deep=True)
+    board.players = board.players.copy(update={order.type: join_player}, deep=True)
     return board.players
 
 
-@router.delete('/{order}', response_model=Players)
-async def leave_player(order: OrderType, param: PlayersParam = Depends()) -> Players:
+@router.delete('/', response_model=Players)
+async def leave_player(param: PlayersParam = Depends()) -> Players:
     leave_player, board = param.player, param.board
+    order = leave_player.order
     if order == OrderType.NONE:
-        raise HTTPException(status_code=400, detail='request order is none')
+        raise HTTPException(status_code=400, detail='Request order is none')
 
-    board_player = get_player_from_order_in_board(order, param.board)
+    board_player = get_player_from_order_in_board(order, board)
     if board_player is None:
         raise HTTPException(status_code=400, detail='There is no player')
     elif board_player != leave_player:
         raise HTTPException(status_code=400, detail='player does not match')
 
-    board.players = board.players.copy(update={order: None})
+    board.players = board.players.copy(update={order.type: None})
     return board.players
