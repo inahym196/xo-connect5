8efa410b644609736a9aa89617a7960a793adee0
inahym196 <user@USERnoMacBook-Pro.local>
2022-08-29 16:01:31 +0900

    implement-players-endpoint-and-restructure

--- src/xo_connect5/routers/boards/boards.py
+++ src/xo_connect5/routers/boards/boards.py
@@ -8,6 +8,11 @@ router = APIRouter()
 
 boards = Boards()
 
+# dev-init
+_init_pieces = [[PieceType.NONE for j in range(10)] for i in range(10)]
+_board = Board(id=0, pieces=_init_pieces)
+boards.items.append(_board)
+
 
 @router.get('/')
 async def get_boards() -> Boards:
