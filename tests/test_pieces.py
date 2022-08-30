182710b57e586fff32a71e11521de63e8cc9477e
inahym196 <user@USERnoMacBook-Pro.local>
2022-08-30 14:14:58 +0900

    unuse-fixture

--- tests/test_pieces.py
+++ tests/test_pieces.py
@@ -1,25 +1,9 @@
 
 
-import pytest
 from requests.models import Response
-from xo_connect5.models.boards import Board, BoardStatus
 from xo_connect5.models.pieces import Pieces, PieceType
-from xo_connect5.models.users import Players, User
 
 from tests import client, http_exception_404
-
-
-@pytest.fixture
-def pieces():
-    pieces = [[PieceType.NONE for j in range(10)] for i in range(10)]
-    return pieces
-
-
-@pytest.fixture
-def board():
-    players = Players(first=User(name='first'), draw=User(name='draw'))
-    board = Board(id=0, players=players, status=BoardStatus.STARTING)
-    return board
 
 
 class TestPieces:
