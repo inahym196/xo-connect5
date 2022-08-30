7bc025d740478183f699bb8a52c811551c582f26
inahym196 <user@USERnoMacBook-Pro.local>
2022-08-30 12:00:47 +0900

    add-test

--- tests/test_boards.py
+++ tests/test_boards.py
@@ -1,5 +1,6 @@
 
 
+import pytest
 from fastapi.exceptions import HTTPException
 from fastapi.testclient import TestClient
 from requests.models import Response
@@ -8,6 +9,13 @@ from xo_connect5.models.boards import Board, Boards, BoardStatus
 from xo_connect5.models.users import Players, User
 
 client = TestClient(app)
+
+
+@pytest.fixture
+def board():
+    players = Players(first=User(name='first'), draw=User(name='draw'))
+    board = Board(id=0, players=players, status=BoardStatus.STARTING)
+    return board
 
 
 def test_get_boards():
@@ -25,3 +33,44 @@ def test_post_boards():
     boards = HTTPException(status_code=409)
     assert response.status_code == boards.status_code
     assert response.json()['detail'] == boards.detail
+
+
+def test_get_board(board: Board):
+    response: Response = client.get('/api/v1/boards/0/')
+
+    assert response.status_code == 200
+    assert response.json() == board
+
+
+def test_get_board_fail():
+    response: Response = client.get('/api/v1/boards/1/')
+    exception = HTTPException(status_code=404)
+
+    assert response.status_code == exception.status_code
+    assert response.json()['detail'] == exception.detail
+
+
+def test_get_board_status(board: Board):
+    response: Response = client.get('/api/v1/boards/0/status')
+
+    assert response.status_code == 200
+    assert response.json() == board.status
+
+
+def test_delete_board():
+    response: Response = client.delete('/api/v1/boards/0')
+    exception = HTTPException(status_code=204)
+
+    assert response.status_code == exception.status_code
+    assert response._content == b''
+
+
+def test_post_board(board: Board):
+    response: Response = client.post('/api/v1/boards/')
+
+    assert response.status_code == 200
+    assert response.json()['id'] == board.id
+    assert response.json()['status'] == BoardStatus.WAITING
+    assert response.json()['pieces'] == board.pieces
+    assert response.json()['players'] == {'first': None, 'draw': None}
+    assert response.json()['round'] == board.round
