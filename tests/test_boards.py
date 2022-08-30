331868e0e898a4449b812dba26507941ad6593dd
inahym196 <user@USERnoMacBook-Pro.local>
2022-08-30 12:08:31 +0900

    classify

--- tests/test_boards.py
+++ tests/test_boards.py
@@ -18,59 +18,58 @@ def board():
     return board
 
 
-def test_get_boards():
-    response: Response = client.get('/api/v1/boards/')
-    _players = Players(first=User(name='first'), draw=User(name='draw'))
-    _board = Board(id=0, players=_players, status=BoardStatus.STARTING)
-    boards = Boards(items=[_board])
+class TestBoards:
 
-    assert response.status_code == 200
-    assert response.json()['items'] == boards.items
+    def test_get_boards(self):
+        response: Response = client.get('/api/v1/boards/')
+        _players = Players(first=User(name='first'), draw=User(name='draw'))
+        _board = Board(id=0, players=_players, status=BoardStatus.STARTING)
+        boards = Boards(items=[_board])
 
+        assert response.status_code == 200
+        assert response.json()['items'] == boards.items
 
-def test_post_boards():
-    response: Response = client.post('/api/v1/boards/')
-    boards = HTTPException(status_code=409)
-    assert response.status_code == boards.status_code
-    assert response.json()['detail'] == boards.detail
-
-
-def test_get_board(board: Board):
-    response: Response = client.get('/api/v1/boards/0/')
-
-    assert response.status_code == 200
-    assert response.json() == board
+    def test_post_boards(self):
+        response: Response = client.post('/api/v1/boards/')
+        boards = HTTPException(status_code=409)
+        assert response.status_code == boards.status_code
+        assert response.json()['detail'] == boards.detail
 
 
-def test_get_board_fail():
-    response: Response = client.get('/api/v1/boards/1/')
-    exception = HTTPException(status_code=404)
+class TestBoard:
 
-    assert response.status_code == exception.status_code
-    assert response.json()['detail'] == exception.detail
+    def test_get_board(self, board: Board):
+        response: Response = client.get('/api/v1/boards/0/')
 
+        assert response.status_code == 200
+        assert response.json() == board
 
-def test_get_board_status(board: Board):
-    response: Response = client.get('/api/v1/boards/0/status')
+    def test_get_board_fail(self):
+        response: Response = client.get('/api/v1/boards/1/')
+        exception = HTTPException(status_code=404)
 
-    assert response.status_code == 200
-    assert response.json() == board.status
+        assert response.status_code == exception.status_code
+        assert response.json()['detail'] == exception.detail
 
+    def test_get_board_status(self, board: Board):
+        response: Response = client.get('/api/v1/boards/0/status')
 
-def test_delete_board():
-    response: Response = client.delete('/api/v1/boards/0')
-    exception = HTTPException(status_code=204)
+        assert response.status_code == 200
+        assert response.json() == board.status
 
-    assert response.status_code == exception.status_code
-    assert response._content == b''
+    def test_delete_board(self):
+        response: Response = client.delete('/api/v1/boards/0')
+        exception = HTTPException(status_code=204)
 
+        assert response.status_code == exception.status_code
+        assert response._content == b''
 
-def test_post_board(board: Board):
-    response: Response = client.post('/api/v1/boards/')
+    def test_post_board(self, board: Board):
+        response: Response = client.post('/api/v1/boards/')
 
-    assert response.status_code == 200
-    assert response.json()['id'] == board.id
-    assert response.json()['status'] == BoardStatus.WAITING
-    assert response.json()['pieces'] == board.pieces
-    assert response.json()['players'] == {'first': None, 'draw': None}
-    assert response.json()['round'] == board.round
+        assert response.status_code == 200
+        assert response.json()['id'] == board.id
+        assert response.json()['status'] == BoardStatus.WAITING
+        assert response.json()['pieces'] == board.pieces
+        assert response.json()['players'] == {'first': None, 'draw': None}
+        assert response.json()['round'] == board.round
