db6420356bac39aa73ba6b6f29ae0056a9293ee0
inahym196 <user@USERnoMacBook-Pro.local>
2022-08-30 10:54:16 +0900

    merge-boards-board

--- src/xo_connect5/routers/boards/__init__.py
+++ src/xo_connect5/routers/boards/__init__.py
@@ -1,9 +1,8 @@
 
 from fastapi import APIRouter
-from xo_connect5.routers.boards import board, boards, pieces, players
+from xo_connect5.routers.boards import boards, pieces, players
 
 router = APIRouter()
 router.include_router(router=boards.router, tags=['boards'])
-router.include_router(router=board.router, prefix='/{board_id}', tags=['board'])
 router.include_router(router=pieces.router, prefix='/{board_id}/pieces', tags=['pieces'])
 router.include_router(router=players.router, prefix='/{board_id}/players', tags=['players'])
