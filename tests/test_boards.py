4b9d3486066583bc9500a975027fae3a8041d9b7
inahym196 <user@USERnoMacBook-Pro.local>
2022-08-30 10:45:22 +0900

    replace-starlette2fastapi-exception

--- tests/test_boards.py
+++ tests/test_boards.py
@@ -1,8 +1,8 @@
 
 
+from fastapi.exceptions import HTTPException
 from fastapi.testclient import TestClient
 from requests.models import Response
-from starlette.exceptions import HTTPException
 from xo_connect5.main import app
 from xo_connect5.models.boards import Board, Boards, BoardStatus
 from xo_connect5.models.users import Players, User
@@ -11,18 +11,17 @@ client = TestClient(app)
 
 
 def test_get_boards():
-    response: Response = client.get('/api/v1/boards')
-    expected = Boards()
+    response: Response = client.get('/api/v1/boards/')
     _players = Players(first=User(name='first'), draw=User(name='draw'))
     _board = Board(id=0, players=_players, status=BoardStatus.STARTING)
-    expected.items.append(_board)
+    boards = Boards(items=[_board])
 
     assert response.status_code == 200
-    assert response.json()['items'] == expected.items
+    assert response.json()['items'] == boards.items
 
 
 def test_post_boards():
     response: Response = client.post('/api/v1/boards/')
-    expected = HTTPException(status_code=409)
-    assert response.status_code == expected.status_code
-    assert response.json()['detail'] == expected.detail
+    boards = HTTPException(status_code=409)
+    assert response.status_code == boards.status_code
+    assert response.json()['detail'] == boards.detail
