7ffc55df9627dbd6112d8968bfd738c1b39cf571
inahym196 <user@USERnoMacBook-Pro.local>
2022-09-01 02:32:48 +0900

    add-test-put-pieces

--- tests/test_pieces.py
+++ tests/test_pieces.py
@@ -1,18 +1,20 @@
 
 
+import copy
+
+import pytest
 from requests.models import Response
 
 from tests import client
 from tests.helpers import assert_equal_players_exception
 
+init_pieces = [['_' for j in range(10)] for i in range(10)]
+
 
 class TestGetPieces:
-
     def test_get_pieces_when_init_board(self, init_board):
         response: Response = client.get('/api/v1/boards/0/pieces/')
-        pieces = [['_' for j in range(10)] for i in range(10)]
-
-        assert response.json() == pieces
+        assert response.json() == init_pieces
 
 
 class TestPutPieces:
@@ -25,3 +27,26 @@ class TestPutPieces:
         }
         response: Response = client.patch('/api/v1/boards/0/pieces/', params=params, json=data)
         assert_equal_players_exception(response, 400, detail='There is no user on board')
+
+    def test_put_pieces_when_unmatched_user(self, starting_board):
+        params = {'raw': 0, 'column': 0}
+        data = {
+            'user': {'name': 'unmatched_name'},
+            'order': {'type': 'first'}
+        }
+        response: Response = client.patch('/api/v1/boards/0/pieces/', params=params, json=data)
+        assert_equal_players_exception(response, 400, detail='User does not match')
+
+    @pytest.mark.parametrize('raw, column', [(0, 0)])
+    def test_put_pieces_when_matched_user(self, starting_board, raw, column):
+        params = {'raw': raw, 'column': column}
+        data = {
+            'user': {'name': 'first'},
+            'order': {'type': 'first'}
+        }
+        response: Response = client.patch('/api/v1/boards/0/pieces/', params=params, json=data)
+        pieces = copy.copy(init_pieces)
+        pieces[column][raw] = 'xg'
+        starting_board['pieces'] = pieces
+        assert response.status_code == 200
+        assert response.json() == starting_board
