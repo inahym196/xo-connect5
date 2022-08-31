811f2576bdf976d93dd1f8465e9be4e9a2b76b48
inahym196 <user@USERnoMacBook-Pro.local>
2022-08-31 22:15:11 +0900

    add-test-helper

--- tests/test_boards.py
+++ tests/test_boards.py
@@ -1,67 +1,81 @@
 
 from fastapi.exceptions import HTTPException
 from requests.models import Response
-from xo_connect5.models.boards import Board, Boards, BoardStatus
+from xo_connect5.models.boards import Board, BoardStatus
 
-from tests import client, http_exception_404, http_exception_409
+from tests import client
+from tests.helpers import (assert_equal_http_exception_404,
+                           assert_equal_http_exception_409)
 
 
 class TestGetBoards:
 
     def test_get_boards_when_no_board(self, no_board):
         response: Response = client.get('/api/v1/boards/')
-        boards = Boards()
 
         assert response.status_code == 200
-        assert response.json()['items'] == boards.items
+        assert response.json()['items'] == []
 
     def test_get_boards_when_init_board(self, init_board):
         response: Response = client.get('/api/v1/boards/')
-        boards = Boards(items=[Board(id=0)])
 
+        expected_json = {'items': [
+            {
+                'id': 0,
+                'pieces': [['_' for j in range(10)] for i in range(10)],
+                'round': 0,
+                'status': 'waiting',
+                'players': {'first': None, 'draw': None},
+            }
+        ]}
         assert response.status_code == 200
-        for res_item, board_item in zip(response.json()['items'], boards.items):
-            assert res_item == board_item
+        assert response.json() == expected_json
 
 
 class TestPostBoards:
+
     def test_post_boards_when_no_board(self, no_board):
         response: Response = client.post('/api/v1/boards/')
-        board = Board(id=0)
 
+        expected_json = {
+            'id': 0,
+            'pieces': [['_' for j in range(10)] for i in range(10)],
+            'round': 0,
+            'status': 'waiting',
+            'players': {'first': None, 'draw': None},
+        }
         assert response.status_code == 200
-        assert response.json() == board
+        assert response.json() == expected_json
 
     def test_post_boards_when_init_board(self, init_board):
         response: Response = client.post('/api/v1/boards/')
-
-        assert response.status_code == http_exception_409.status_code
-        assert response.json()['detail'] == http_exception_409.detail
+        assert_equal_http_exception_409(response)
 
 
 class TestGetBoard:
 
     def test_get_board_when_no_board(self, no_board):
         response: Response = client.get('/api/v1/boards/0/')
-
-        assert response.status_code == http_exception_404.status_code
-        assert response.json()['detail'] == http_exception_404.detail
+        assert_equal_http_exception_404(response)
 
     def test_get_board_when_init_board(self, init_board):
         response: Response = client.get('/api/v1/boards/0/')
-        board = Board(id=0)
 
+        expected_json = {
+            'id': 0,
+            'pieces': [['_' for j in range(10)] for i in range(10)],
+            'round': 0,
+            'status': 'waiting',
+            'players': {'first': None, 'draw': None},
+        }
         assert response.status_code == 200
-        assert response.json() == board
+        assert response.json() == expected_json
 
 
 class TestGetBoardStatus:
     def test_get_board_status_when_no_board(self, no_board):
         response: Response = client.get('/api/v1/boards/0/status')
-        exception = HTTPException(status_code=404)
-
-        assert response.status_code == exception.status_code
-        assert response.json()['detail'] == exception.detail
+        assert_equal_http_exception_404(response)
 
     def test_get_board_status_when_init_board(self, init_board):
         response: Response = client.get('/api/v1/boards/0/status')
@@ -87,16 +101,13 @@ class TestPostBoard:
 
     def test_post_board_when_init_board(self, init_board):
         response: Response = client.post('/api/v1/boards/')
-
-        assert response.status_code == http_exception_409.status_code
-        assert response.json()['detail'] == http_exception_409.detail
+        assert_equal_http_exception_409(response)
 
 
 class TestDeleteBoard:
     def test_delete_board_when_no_board(self, no_board):
         response: Response = client.delete('/api/v1/boards/0')
-        assert response.status_code == http_exception_404.status_code
-        assert response.json()['detail'] == http_exception_404.detail
+        assert_equal_http_exception_404(response)
 
     def test_delete_board_when_init_board(self, init_board):
         response: Response = client.delete('/api/v1/boards/0')
