f55c20793df4f311dd80a6766fe4c7d0533315b4
inahym196 <user@USERnoMacBook-Pro.local>
2022-09-02 05:19:38 +0900

    fix/router-bug

--- src/xo_connect5/routers/boards/__init__.py
+++ src/xo_connect5/routers/boards/__init__.py
@@ -4,6 +4,6 @@ from xo_connect5.routers.boards import board, boards, pieces, players
 
 router = APIRouter()
 router.include_router(router=boards.router, tags=['boards'])
-router.include_router(router=board.router, tags=['board'])
+router.include_router(router=board.router, prefix='/{board_id}', tags=['board'])
 router.include_router(router=players.router, prefix='/{board_id}/players', tags=['players'])
 router.include_router(router=pieces.router, prefix='/{board_id}/pieces', tags=['pieces'])
