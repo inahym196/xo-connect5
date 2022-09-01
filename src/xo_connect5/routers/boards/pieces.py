31868f3479f9cf87a39708415d7ab42abba863ee
inahym196 <user@USERnoMacBook-Pro.local>
2022-09-02 04:52:54 +0900

    separate-board-boards

--- src/xo_connect5/routers/boards/pieces.py
+++ src/xo_connect5/routers/boards/pieces.py
@@ -5,7 +5,7 @@ from fastapi import APIRouter, Depends
 from xo_connect5.models import Point
 from xo_connect5.models.boards import Board
 from xo_connect5.models.pieces import Pieces
-from xo_connect5.routers.boards.boards import _get_board
+from xo_connect5.routers.boards.board import _get_board
 from xo_connect5.routers.boards.players import PlayersParam
 
 router = APIRouter()
@@ -16,7 +16,7 @@ async def get_pieces(board: Board = Depends(_get_board)) -> Pieces:
     return board.pieces
 
 
-@router.patch('/')
+@router.put('/')
 async def put_piece(point: Point = Depends(), players_param: PlayersParam = Depends()) -> Board:
     order, board = players_param.order, players_param.board
     _ = players_param.get_matched_user()
