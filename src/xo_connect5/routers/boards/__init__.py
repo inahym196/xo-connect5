46b69ecf6b0c99a500fb05ad447b4cda31233ef4
inahym196 <user@USERnoMacBook-Pro.local>
2022-09-02 04:52:54 +0900

    separate-board-boards

--- src/xo_connect5/routers/boards/__init__.py
+++ src/xo_connect5/routers/boards/__init__.py
@@ -1,8 +1,9 @@
 
 from fastapi import APIRouter
-from xo_connect5.routers.boards import boards, pieces, players
+from xo_connect5.routers.boards import board, boards, pieces, players
 
 router = APIRouter()
 router.include_router(router=boards.router, tags=['boards'])
+router.include_router(router=board.router, tags=['board'])
 router.include_router(router=players.router, prefix='/{board_id}/players', tags=['players'])
 router.include_router(router=pieces.router, prefix='/{board_id}/pieces', tags=['pieces'])
