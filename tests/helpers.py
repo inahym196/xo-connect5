b27ba1b7d7a6ec17506c27ab30d7d7370f65e992
inahym196 <user@USERnoMacBook-Pro.local>
2022-09-02 07:06:36 +0900

    fix/delete-debug-print

--- tests/helpers.py
+++ tests/helpers.py
@@ -13,7 +13,6 @@ def assert_equal_http_exception_404(response: Response):
 def assert_equal_http_exception_409(response: Response):
     expected_exception = HTTPException(status_code=409)
     assert response.status_code == expected_exception.status_code
-    print(response.json())
     assert response.json()['detail'] == expected_exception.detail
 
 
