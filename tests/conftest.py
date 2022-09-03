64937b7a44391db9cfcddefc140a1362d831b565
inahym196 <user@USERnoMacBook-Pro.local>
2022-09-03 16:04:45 +0900

    piecetype-none-str-is-no-space

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,7 +23,7 @@ def init_board() -> BoardFixture:
     response = client.post('/api/v1/boards/')
     expected = {
         'id': 0,
-        'pieces': [['_' for j in range(10)] for i in range(10)],
+        'pieces': [['' for j in range(10)] for i in range(10)],
         'turn': 0,
         'status': 'waiting',
         'players': {'first': None, 'draw': None},
